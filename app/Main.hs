42fec6f346112a2ebc0da893d02e349e96ebe366
ChrisHobday <hobday.chris@protonmail.com>
2023-10-09 21:18:36 -0400

    Mess around with getting a keyboard char instantly and without echoing it to stdout.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main (main) where
 
+import Control.Concurrent
+import System.IO
+import System.Console.ANSI
 import TextEditor
 
-main :: IO ()
-main = putStrLn $ text textEditor
+-- checkIfHIsReady = do
+--   isHReady <- hReady stdin
+--   case isHReady of
+--     True -> do
+--       char <- getChar
+--       print char
+--       threadDelay 1000000
+--     False -> do
+--       putStrLn "N/A"
+--   checkIfHIsReady
+
+-- main :: IO ()
+-- main = do
+--   checkIfHIsReady
+
+main = do
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  char <- getChar
+  putStrLn $ "You pressed" <> show char
+  return ()
